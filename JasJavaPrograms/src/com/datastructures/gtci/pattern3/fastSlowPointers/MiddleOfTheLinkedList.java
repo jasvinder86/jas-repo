534164b00edcf112e91d720f1942233199e3e5be
Jasvinder Singh Chugh <jasvinder.chugh@oracle.com>
2021-10-24 19:06:59 +0530

    Program for fast-slow pointer

---
+++ JasJavaPrograms/src/com/datastructures/gtci/pattern3/fastSlowPointers/MiddleOfTheLinkedList.java
@@ -0,0 +1,40 @@
+package com.datastructures.gtci.pattern3.fastSlowPointers;
+
+class ListNode {
+    int data;
+    ListNode next;
+
+    public ListNode(int data) {
+        this.data = data;
+    }
+}
+
+public class MiddleOfTheLinkedList {
+
+    public static ListNode getMiddleNode(ListNode head) {
+
+        ListNode slowPointer = head;
+        ListNode fastPointer = head;
+
+        while (fastPointer != null && fastPointer.next != null) {
+            fastPointer = fastPointer.next.next;
+            slowPointer = slowPointer.next;
+        }
+        return slowPointer;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        System.out.println("Middle node is " + MiddleOfTheLinkedList.getMiddleNode(head).data);
+
+        head.next.next.next.next.next = new ListNode(6);
+        System.out.println("Middle node is " + MiddleOfTheLinkedList.getMiddleNode(head).data);
+
+        head.next.next.next.next.next.next = new ListNode(7);
+        System.out.println("Middle node is " + MiddleOfTheLinkedList.getMiddleNode(head).data);
+    }
+}
